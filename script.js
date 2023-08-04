46a3254f569597feec20efaa291cf93259ce5cd1
Abhishek-aks <117778085+Abhishek-aks@users.noreply.github.com>
2023-08-04 21:11:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your code here
+const nameInput = document.getElementById("name");
+const scoreInput = document.getElementById("score");
+const scores = document.getElementById("scores");
+
+// Save score to Local Storage
+function saveScore() {
+  // complete the code here
+  showScores();
+}
+
+// Show scores in div
+function showScores() {
+  // complete the code
+}
